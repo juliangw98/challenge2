1fe0438ae52e278906b75913f51e1f2dc394aff6
julian <julian.gio.wieckardt@gmail.com>
2018-03-11 23:47:42 +0100

    wat te doen button

--- javascript.js
+++ javascript.js
@@ -29,10 +29,10 @@ function startTime() {
     }
     else if (h >= 19 && h <= 23) {
         document.getElementById("flexContainer").style.backgroundColor = 'purple';
-        document.getElementById("whatToDo").innerHTML = 'Evening life ';
+        document.getElementById("whatToDo").innerHTML = 'Night life ';
     }
     else {
-        document.getElementById("flexContainer").style.backgroundColor = 'darkblue';
+        document.getElementById("flexContainer").style.backgroundColor = 'blue';
         document.getElementById("whatToDo").innerHTML = 'Good Night zzzzz';
     }
 }
@@ -44,4 +44,31 @@ function checkTime(i) {
     return i;
 }
 
-function changeBackground() {}
+function doWhat() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+    if (h >= 7 && h <= 9) {
+        document.getElementById('Antwoord').innerHTML = 'neem een kop koffie en ontbijt!'
+    }
+    else if (h >= 10 && h <= 12) {
+      document.getElementById('Antwoord').innerHTML = 'doe wat extra werk misschien krijg je wel opslag van je baas!'
+    }
+    else if (h >= 13 && h <= 14) {
+      document.getElementById('Antwoord').innerHTML = 'even genoeg gedaan op werk kijk hoe het gaat met je laatste insta post'
+    }
+    else if (h >= 15 && h <= 18) {
+       document.getElementById('Antwoord').innerHTML = 'werk moet maar bedenk alvast wat je wilt eten vanavond'
+    }
+    else if (h >= 19 && h <= 23) {
+       document.getElementById('Antwoord').innerHTML = 'kijk een film'
+    }
+    else {
+        document.getElementById('Antwoord').innerHTML = 'Beste tijd om te gaan tukken. maar als je wakker blijft, het is happy hour bij de bar ! Go Party!!!! '
+    }
+}
