27217f6d6683269efd17f9061d86111cf7095f8a
juliangw98 <36408849+juliangw98@users.noreply.github.com>
2018-03-19 11:03:12 +0100

    alleen nog responsive maken

--- javascript.js
+++ javascript.js
@@ -12,28 +12,34 @@ function startTime() {
     document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
     if (h >= 7 && h <= 9) {
-        document.getElementById("flexContainer").style.backgroundColor = '#83EAE2';
+        document.getElementById("flexContainer").style.backgroundImage =  'url(images/' + 'morning' + '.jpeg)';
         document.getElementById("whatToDo").innerHTML = 'Good morning rise and shine';
-    }
-    else if (h >= 10 && h <= 12) {
-        document.getElementById("flexContainer").style.backgroundColor = '#30232F';
-        document.getElementById("whatToDo").innerHTML = 'you just gotta go to work work work work work';
-    }
-    else if (h >= 13 && h <= 14) {
-        document.getElementById("flexContainer").style.backgroundColor = ' #373B47';
-        document.getElementById("whatToDo").innerHTML = 'have a nice break';
-    }
-    else if (h >= 15 && h <= 18) {
-        document.getElementById("flexContainer").style.backgroundColor = '#59838C';
-        document.getElementById("whatToDo").innerHTML = 'Work work work work work';
-    }
-    else if (h >= 19 && h <= 23) {
-        document.getElementById("flexContainer").style.backgroundColor = '#71E5F4';
-        document.getElementById("whatToDo").innerHTML = 'Night life ';
-    }
-    else {
-        document.getElementById("flexContainer").style.backgroundColor = '#191323';
-        document.getElementById("whatToDo").innerHTML = 'Good Night zzzzz';
+        
+
+    } else if (h >= 10 && h <= 12) {
+        document.getElementById("flexContainer").style.backgroundImage =  'url(images/' + 'working1' + '.jpeg)';
+        document.getElementById("whatToDo").innerHTML = 'Tijd voor werk';
+        
+
+    } else if (h >= 13 && h <= 14) {
+        document.getElementById("flexContainer").style.backgroundImage =  'url(images/' + 'lunch' + '.jpeg)';
+        document.getElementById("whatToDo").innerHTML = 'Even pauze';
+       
+
+    } else if (h >= 15 && h <= 18) {
+        document.getElementById("flexContainer").style.backgroundImage =  'url(images/' + 'work2' + '.jpeg)';
+        document.getElementById("whatToDo").innerHTML = 'En door';
+       
+    } else if (h >= 19 && h <= 23) {
+        document.getElementById("flexContainer").style.backgroundImage =  'url(images/' + 'dinner' + '.jpeg)';
+        document.getElementById("whatToDo").innerHTML = 'Tijd voor jezeld ';
+        
+
+    } else {
+        document.getElementById("flexContainer").style.backgroundImage =  'url(images/' + 'uitgaan' + '.jpeg)';
+
+        document.getElementById("whatToDo").innerHTML = 'Slaapzacht.. of niet';
+        
     }
 }
 
@@ -54,21 +60,36 @@ function doWhat() {
     document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
     if (h >= 7 && h <= 9) {
-        document.getElementById('Antwoord').innerHTML = 'neem een kop koffie en ontbijt!'
-    }
-    else if (h >= 10 && h <= 12) {
-      document.getElementById('Antwoord').innerHTML = 'doe wat extra werk misschien krijg je wel opslag van je baas!'
-    }
-    else if (h >= 13 && h <= 14) {
-      document.getElementById('Antwoord').innerHTML = 'even genoeg gedaan op werk kijk hoe het gaat met je laatste insta post'
-    }
-    else if (h >= 15 && h <= 18) {
-       document.getElementById('Antwoord').innerHTML = 'werk moet maar bedenk alvast wat je wilt eten vanavond'
-    }
-    else if (h >= 19 && h <= 23) {
-       document.getElementById('Antwoord').innerHTML = 'kijk een film'
-    }
-    else {
-        document.getElementById('Antwoord').innerHTML = 'Beste tijd om te gaan tukken. maar als je wakker blijft, het is happy hour bij de bar ! Go Party!!!! '
+        document.getElementById('Antwoord').innerHTML = 'neem een kop koffie en ontbijt!';
+        TweenMax.to('#Antwoord',2,{
+            left:500
+        });
+    } else if (h >= 10 && h <= 12) {
+        document.getElementById('Antwoord').innerHTML = 'doe wat extra werk misschien krijg je wel opslag van je baas!';
+        TweenMax.to('#Antwoord',2,{
+                top:500
+        })
+    } else if (h >= 13 && h <= 14) {
+        document.getElementById('Antwoord').innerHTML = 'even genoeg gedaan op werk kijk hoe het gaat met je laatste insta post';
+        TweenMax.to('#Antwoord',2,{
+            left:500
+        })
+    } else if (h >= 15 && h <= 18) {
+        document.getElementById('Antwoord').innerHTML = 'werk moet maar bedenk alvast wat je wilt eten vanavond';
+        TweenMax.to('#Antwoord',2,{
+            left:500
+        })
+    } else if (h >= 19 && h <= 23) {
+        document.getElementById('Antwoord').innerHTML = 'kijk een film';
+        TweenMax.to('#Antwoord',2,{
+            left:500
+        })
+    } else {
+        document.getElementById('Antwoord').innerHTML = 'Beste tijd om te gaan tukken. maar als je wakker blijft, het is happy hour bij de bar !Go Party!!!! ' ;
+        TweenMax.to('#Antwoord',2,{
+            left:500
+        })
     }
 }
+
+function shrink(){}
